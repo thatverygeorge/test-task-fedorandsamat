69fda101c9ad3d7d838f7764b12e1485c337e4cb
George Kirakosyan <georgiikirakosyan@mail.ru>
2024-02-13 12:31:22 +0300

    increase e2e testing timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './src/__tests__/e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 10 * 1000,
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
