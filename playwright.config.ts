59eb7b3e7c253ae544b3fcfcf2701eb70ad55a4e
George Kirakosyan <georgiikirakosyan@mail.ru>
2024-02-13 20:09:44 +0300

    try different command for playwright web server on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -104,7 +104,7 @@ export default defineConfig({
      * Use the preview server on CI for more realistic testing.
      * Playwright will re-use the local server if there is already a dev-server running.
      */
-    command: process.env.CI ? 'npx vite preview --port 5173' : 'npx vite dev',
+    command: process.env.CI ? 'npm run build && npx vite preview --port 5173' : 'vite dev',
     port: 5173,
     reuseExistingServer: !process.env.CI,
   },
